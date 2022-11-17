8f21338e9a3a9da6d320691e025c33e2dfa48e32
sucesso <vitorhugoalvesbezerra88@gmail.com>
2022-11-17 16:46:14 -0300

    Adionando os arquivos

---
+++ Tabuada/script.js
@@ -0,0 +1,20 @@
+function tabuada(){
+    let num = document.getElementById('txtn') 
+    var tab = document.getElementById('seltab')
+    
+    if(num.value.length == 0){
+        window.alert('[ERRO]')
+    }else{
+        var n = Number(num.value)
+        var c = 1 
+        tab.innerHTML = ''
+        while(c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} + ${c} = ${c + n}`
+            tab.appendChild(item)
+            c++ 
+       }
+
+    }
+    
+}
